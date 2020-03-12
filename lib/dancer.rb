680b9ca62b04393d3fc2c1c31e4065284492d0c8
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-12 23:40:05 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,8 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-
 class Dancer
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+  end
 
 
 end
