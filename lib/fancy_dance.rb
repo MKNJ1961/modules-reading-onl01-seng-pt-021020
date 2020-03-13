c2568f7bef4836d2eac9941a6869b694f3959121
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-13 00:31:24 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -20,6 +20,17 @@ module FancyDance
 
   module ClassMethods
 
+  class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
+
+  class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
+    
+
     def metadata
       "This class produces objects that love to dance."
     end
