a1f737b23d416bcef7fdbf816cb1e893b288eea7
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-13 00:23:52 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,9 +3,12 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
-
 end
 
 class Kid
+  extend MetaDancing
+end
+
+class Dancer
   extend MetaDancing
 end
